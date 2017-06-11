f8df6d25cf66c92bbfab1f15a3b79f6a082036c7
potenn <poten1129@gmail.com>
2017-06-12 02:08:10 +0900

    add validAnagram problem

---
+++ ValidAnagram/Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+
+    public boolean isAnagram(String s, String t) {
+
+    	if(s.length() != t.length()){
+			return false;
+		}
+
+
+    //아스키 코드값을 기준으로 일치여부를 판단할겁니다.
+		int s1Arr[] = new int[128];
+		int s2Arr[] = new int[128];
+
+		for(char c : s.toCharArray()){
+			s1Arr[c]++;
+		}
+		for(char c : t.toCharArray()){
+			s2Arr[c]++;
+		}
+		//여기서 즉
+		for(int i = 0 ; i < s1Arr.length ; i++){
+			if(s1Arr[i] != s2Arr[i]){
+				return false;
+			}
+		}
+
+		return true;
+    }
+
+}
